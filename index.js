616309c763f85486a5747e25b9ae5a9083c36cb2
michaelcchu <michael02chu@gmail.com>
2023-02-15 13:32:33 -0500

    made the two canvases have the same width ...
    ... and height. also added code to find non-white pixels in the score.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ loadingTask.promise.then(function(pdf) {
   // you can now use *pdf* here
   pdf.getPage(1).then(function(page) {
     // you can now use *page* here
-    var scale = 1.5;
+    var scale = 1;
     var viewport = page.getViewport({ scale: scale, });
     // Support HiDPI-screens.
     var outputScale = window.devicePixelRatio || 1;
@@ -13,21 +13,23 @@ loadingTask.promise.then(function(pdf) {
 
     canvas.width = Math.floor(viewport.width * outputScale);
     canvas.height = Math.floor(viewport.height * outputScale);
-    canvas.style.width = Math.floor(viewport.width) + "px";
-    canvas.style.height =  Math.floor(viewport.height) + "px";
 
     var transform = outputScale !== 1
     ? [outputScale, 0, 0, outputScale, 0, 0]
     : null;
 
     var renderContext = {
-    canvasContext: context,
-    transform: transform,
-    viewport: viewport
+        canvasContext: context,
+        transform: transform,
+        viewport: viewport
     };
-    page.render(renderContext);
 
-    createCursor();
+    var renderingTask = page.render(renderContext);
+
+    renderingTask.promise.then(function() {    
+        const cursor = createCursor();
+        showImageData(cursor);
+    });
   });
 });
 
@@ -77,6 +79,73 @@ function createCursor() {
             cursor.update();
         }
     });
+
+    return cursor;
 }
 
+function showImageData(cursor) {
+    const canvas = document.getElementById('layer1');
+    const context = canvas.getContext('2d');
 
+    const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imgData.data;
+    console.log(imgData);
+
+    let i = 0;
+    let count = 0;
+    let whiteCount = 0;
+    while (4*i < data.length) {
+        const red = data[4*i];
+        const green = data[4*i + 1];
+        const blue = data[4*i + 2];
+        const alpha = data[4*i + 3];
+    
+        if ((data[4*i] !== 255) || 
+            (data[4*i+1] !== 255) || 
+            (data[4*i+2] !== 255)) {
+            count += 1;
+            row = i / canvas.width;
+            column = i % canvas.width;
+
+            cursor.clear();
+            cursor.y = row;
+            cursor.x = column;
+            cursor.update();
+
+            break;
+        } else {
+            whiteCount += 1;
+        }
+        i += 1;
+    }
+    const totalPixels = count+whiteCount;
+    console.log(count, count / totalPixels);
+    console.log(whiteCount, whiteCount / totalPixels);
+    console.log(totalPixels);
+}
+
+let i = 0;
+
+function advanceOnePixel() {
+    const canvas = document.getElementById('layer1');
+    const context = canvas.getContext('2d');
+
+    const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imgData.data;
+    
+    if (i < data.length) {
+        const red = data[i];
+        const green = data[i + 1];
+        const blue = data[i + 2];
+        const alpha = data[i + 3];
+    
+        data[i] = 0
+        data[i+1] = 255;
+        data[i+2] = 0;
+
+        context.putImageData(imgData, 0, 0);
+        i += 4;
+    }
+}
+
+document.addEventListener("keydown", advanceOnePixel);
